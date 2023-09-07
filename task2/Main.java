5f2ddb159cc924e35462d45ff4e4bc7c759ccd72
DmitriyPanfilov <126699728+DmitriyPanfilov@users.noreply.github.com>
2023-09-07 09:19:58 +0300

    Task 2

---
+++ task2/Main.java
@@ -0,0 +1,14 @@
+package lesson_2.homework2.task2;
+
+public class Main {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+
+        double sum = rectangle.calculateArea(2,6);
+        double hye = rectangle.calculatePerimeter(67,7);
+        System.out.println(sum);
+        System.out.println(hye);
+
+        }
+    }
+
---
+++ task2/Rectangle.java
@@ -0,0 +1,12 @@
+package lesson_2.homework2.task2;
+
+class Rectangle implements Shape{
+
+
+    @Override
+    public double calculateArea(double width, double height){
+        return width * height;
+    }
+
+
+}
